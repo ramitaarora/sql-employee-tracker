92aa45d60df8334ac3d4c8feb905e48bb806f657
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-10-12 12:56:43 -0700

    function framework

--- index.js
+++ index.js
@@ -13,25 +13,94 @@ const db = mysql.createConnection(
     console.log(`Connected to the employee_db database.`)
   );
 
+// Table functions
+
 function viewEmployees() {
     db.query(`SELECT * FROM employees`, function (err, res) {
         console.table(res);
     });
+    menu();
+}
+
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "Please enter the employee's first name: ",
+            name: firstName,
+        },
+        {
+            type: 'input',
+            message: "Please enter the employee's last name: ",
+            name: lastName,
+        },
+        {
+            type: 'list',
+            message: 'Please choose a role for the employee: ',
+            choices: [],
+            name: 'role',
+        },
+        {
+            type: 'list',
+            message: 'Please select a manager: ',
+            choices: [],
+            name: 'manager',
+        }
+    ]).then((answers) => {
+        console.log(answers);
+    })
+}
+
+function updateEmployeeRole() {
+
 }
 
 function viewRoles() {
     db.query(`SELECT * FROM roles`, function (err, res) {
         console.table(res);
     });
+    menu();
+}
+
+function addRole() {
+
 }
 
 function viewDepartments() {
     db.query(`SELECT * FROM departments`, function (err, res) {
         console.table(res);
     });
+    menu();
 }
 
-// Function to start questions
+function addDepartment() {
+
+}
+
+
+// Menu function
+
+function menu() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit'],
+            name: 'menuChoice',
+        }
+    ]).then((choice) => {
+            if (choice.menuChoice === 'View All Employees') viewEmployees();
+            if (choice.menuChoice === 'Add Employee') addEmployee();
+            if (choice.menuChoice === 'Update Employee Role') updateEmployeeRole();
+            if (choice.menuChoice === 'View All Roles') viewRoles();
+            if (choice.menuChoice === 'Add role') addRole();
+            if (choice.menuChoice === 'View All Departments') viewDepartments();
+            if (choice.menuChoice === 'Add Departments') addDepartment();
+            if (choice.menuChoice === 'Quit') process.exit();
+        });
+}
+
+// Function to start program
 
 function init() {
     console.log(`
@@ -44,18 +113,7 @@ function init() {
     |________________________|
 
     `);
-    inquirer.prompt([
-        {
-            type: 'list',
-            message: 'What would you like to do?',
-            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department'],
-            name: 'menuChoice',
-        }
-    ]).then((choice) => {
-            if (choice.menuChoice === 'View All Employees') viewEmployees();
-            if (choice.menuChoice === 'View All Roles') viewRoles();
-            if (choice.menuChoice === 'View All Departments') viewDepartments();
-        });
+    menu();
 }
 
 // Function call to initialize app
