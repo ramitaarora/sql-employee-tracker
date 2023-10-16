74ea5a42e633a117989a73752c7d1880f9b6239f
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-10-15 18:47:44 -0700

    folder structure

--- db/schema.sql
+++ db/schema.sql
@@ -7,12 +7,6 @@ CREATE TABLE departments (
     name VARCHAR(50) NOT NULL
 );
 
-INSERT INTO departments (name)
-VALUES ('Sales'),
-    ('Engineering'),
-    ('Finance'),
-    ('Legal');
-
 CREATE TABLE roles (
     role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     role VARCHAR(50) NOT NULL,
@@ -20,16 +14,6 @@ CREATE TABLE roles (
     department_id INT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES departments(department_id)
 );
-
-INSERT INTO roles (role, salary, department_id)
-VALUES ('Sales Lead', 100000, 1),
-    ('Salesperson', 80000, 1),
-    ('Lead Engineer', 150000, 2),
-    ('Software Engineer', 12000, 2),
-    ('Account Manager', 160000, 3),
-    ('Accountant', 125000, 3),
-    ('Legal Team Lead', 250000, 4),
-    ('Lawyer', 190000, 4);
 
 CREATE TABLE employees (
     employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -40,13 +24,3 @@ CREATE TABLE employees (
     FOREIGN KEY (role_id) REFERENCES roles(role_id),
     FOREIGN KEY (manager_id) REFERENCES employees (employee_id)
 );
-
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ('John', 'Doe', 1, null),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, null),
-    ('Kevin', 'Tupik', 4, 3),
-    ('Kunal', 'Singh', 5, null),
-    ('Malia', 'Brown', 6, 5),
-    ('Sarah', 'Lourd', 7, null),
-    ('Tom', 'Allen', 8, 7);
